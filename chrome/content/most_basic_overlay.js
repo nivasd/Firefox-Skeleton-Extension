be43d4bbaf96255f471ee39a7f7e1562d9d67cf2
Nivas Durairaj <nivasd@gmail.com>
2011-12-29 22:31:49 -0800

    added url links to basic overlay

--- chrome/content/most_basic_overlay.js
+++ chrome/content/most_basic_overlay.js
@@ -15,6 +15,25 @@ function on_menu_go()
     alert('on menu go ' + text + '\n');
 }
 
+function on_basic_menu_select(idval) {
+   var n = document.getElementById(idval).value;  
+   alert(n); 
+   switch (n)
+   {
+     case 1: 
+             alert("hello Nivas");  
+	     break; 
+     case 2: 
+             break; 
+     default: 
+
+       
+
+   }
+
+
+}
+
 function on_menu_basic_change()
 {
     dump('on menu basic change\n');
